78c7dcdb4aa72794ba3a571ef588bbb5e2603c22
Hyun Kyu Seo <hyunkyuseo@Hyuns-MacBook-Pro-3.local>
2026-01-08 21:37:37 -0500

    naming

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,18 +2,22 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Cesium in Next.js 14 with TypeScript",
-  description: "An example Next.js 14 TypeScript project displaying Cesium hosted in Vercel",
+  title: "Cesium in Next.js 16 with TypeScript",
+  description:
+    "An example Next.js 16 TypeScript project displaying Cesium hosted in Vercel",
   openGraph: {
     type: "website",
-    siteName: "Cesium in Next.js 14 with TypeScript",
-    title: "Cesium in Next.js 14 with TypeScript",
+    siteName: "Cesium in Next.js 16 with TypeScript",
+    title: "Cesium in Next.js 16 with TypeScript",
     url: `https://nextjs-cesium.vercel.app`,
-    description: "An example Next.js 14 TypeScript project displaying Cesium hosted in Vercel",
-    images: [{
-      url: `https://nextjs-cesium.vercel.app/og.png`
-    }]
-  }
+    description:
+      "An example Next.js 16 TypeScript project displaying Cesium hosted in Vercel",
+    images: [
+      {
+        url: `https://nextjs-cesium.vercel.app/og.png`,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
